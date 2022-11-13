56897c77f0ea1f893f11cbb6d7dfc8c23a6ea28a
Nishant <nishants440@gmail.com>
2022-11-13 19:36:46 +0530

    Changes

--- App.js
+++ App.js
@@ -1,11 +1,79 @@
 import * as React from "react";
-import MapView from "react-native-maps";
+import { useState } from "react";
+import MapView, {
+  PROVIDER_GOOGLE,
+  Marker,
+  Callout,
+  Circle,
+  Polygon,
+} from "react-native-maps";
 import { StyleSheet, Text, View, Dimensions } from "react-native";
 
 export default function App() {
+  const [pin, setPin] = useState({ latitude: 37.78825, longitude: -122.4324 });
+  const [polygon, setPolygon] = useState([
+    { latitude: 37.8025259, longitude: -122.4351431 },
+    { latitude: 37.7896386, longitude: -122.421646 },
+    { latitude: 37.7665248, longitude: -122.4161628 },
+    { latitude: 37.7734153, longitude: -122.4577787 },
+    { latitude: 37.7948605, longitude: -122.4596065 },
+    { latitude: 37.8025259, longitude: -122.4351431 },
+  ]);
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker
+          coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
+          draggable={true}
+          onDragStart={(event) =>
+            setPin({
+              latitude: event.nativeEvent.coordinate.latitude,
+              longitude: event.nativeEvent.coordinate.longitude,
+            })
+          }
+          onDragEnd={(event) =>
+            setPin({
+              latitude: event.nativeEvent.coordinate.latitude,
+              longitude: event.nativeEvent.coordinate.longitude,
+            })
+          }
+        >
+          <Callout>
+            <Text>Hello There..</Text>
+          </Callout>
+        </Marker>
+
+        <Circle
+          center={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+          }}
+          radius={1000}
+        />
+        <Polygon
+          coordinates={polygon}
+          strokeColor="#000" // fallback for when `strokeColors` is not supported by the map-provider
+          strokeColors={[
+            "#7F0000",
+            "#00000000", // no color, creates a "long" gradient between the previous and next coordinate
+            "#B24112",
+            "#E5845C",
+            "#238C23",
+            "#7F0000",
+          ]}
+          strokeWidth={3}
+        />
+      </MapView>
     </View>
   );
 }
